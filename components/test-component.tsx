fc659220501cf313ef921b6d35070dd4f305da3d
Ludvig Lindholm <ludviglindholm127@gmail.com>
2023-03-08 11:28:19 +0100

    Deleted files not needed for personal site.

--- components/test-component.tsx
+++
@@ -1,6 +0,0 @@
-export function TestComponent() {
-    return <div>
-        <h3>This is a test component xD.</h3>
-        <p> Dont use this ffs.</p>
-    </div>
-}
--- old_pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "@/styles/Home.module.css"
-
-export default function Home() {
-  return <div className={styles.test_dv}>Welcome to Next.js!</div>
-}

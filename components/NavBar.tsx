8d1411e45f5b641f1ef300fa0ef87b7a3e9c32e1
Ludvig Lindholm <ludviglindholm127@gmail.com>
2023-05-20 14:50:53 +0200

    The page is now much more clean.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,25 +18,26 @@ type Page = {
 
 }
 
+const NavLink = (page: Page): JSX.Element => {
+    return (
+        <a key={page.pk.toString()} href={"/" + page.pk}
+           className="block p-2 rounded-lg bg-gray-700 hover:bg-gray-500 border-stone-700 border-2 no-underline">
+            <div className="font-semibold underline">{page.fields.page_title}</div>
+            <span className="text-sm text-white-500 dark:text-white-400">{page.fields.intro}</span>
+        </a>
+        );
+}
+
 const NavBar  = async () => {
     let pages: Page[] = await getPages();
+
+    let elements = pages.sort((a, b) => a.pk-b.pk).map((page: Page) => {
+        return NavLink(page);
+    })
     return <>
         <div className={styles.text_section}>
-            <div className="grid text-white w-fit rounded-xl mr-4">
-                <ul>
-                    <li className="block p-1 rounded-lg my-2">
-                        <h2>Alla Sidor</h2>
-                    </li>
-                    {pages.sort((a, b) => a.pk-b.pk).map((page: Page) => {
-                        return (<li key={page.pk.toString()}>
-                            <a href={"/" + page.pk}
-                               className="block p-2 rounded-lg bg-gray-700 hover:bg-gray-500 border-stone-700 border-2 no-underline">
-                                <div className="font-semibold underline">{page.fields.page_title}</div>
-                                <span className="text-sm text-white-500 dark:text-white-400">{page.fields.intro}</span>
-                            </a>
-                        </li>);
-                    })}
-                </ul>
+            <div className="flex gap-3 text-white w-fit rounded-xl mr-4">
+                {elements}
             </div>
         </div>
     </>
